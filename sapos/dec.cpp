2502d52a4a3dc5284ab98348d19d16c6d064b0cb
Victor Hugo <vale1c@ufrn.edu.br>
2018-03-21 21:26:40 -0300

    final prestando

--- sapos/dec.cpp
+++ sapos/dec.cpp
@@ -6,5 +6,5 @@ int Sapo::getpulos(){ return pulos;} /**Funcao que retorna a quantidade de pulos
 void Sapo::setpulos(int p){pulos = p;} /**Funcao que determina a quantidade de pulos da classe requisitada*/
 int Sapo::getdistancia(){ return distancia;} /**Funcao que retorna a distância percorrida da classe requisitada*/
 void Sapo::setdistancia(int d){distancia = d;} /**Funcao que determina a distância percorrida da classe requisitada*/
-char Sapo::getname(){return nome;}
+char* Sapo::getname(){return nome;}
 void Sapo::setname(char*name){nome=name;}
--- sapos/imprimir.cpp
+++ sapos/imprimir.cpp
@@ -5,17 +5,17 @@ using namespace std;
 void imprimir(int ganhador, Sapo *sapo){
 	if(ganhador==1){
 		cout <<"***************************************************"<<endl;
-		cout << "O sapo " << sapo[0].getname() << " venceu com " << sapo1.getpulos() <<" pulos e percorreu "<< sapo1.getdistancia()<< " metros" << endl;
+		cout << "O sapo " << sapo[0].getname() << " venceu com " << sapo[0].getpulos() <<" pulos e percorreu "<< sapo[0].getdistancia()<< " metros" << endl;
 		cout <<"***************************************************"<<endl;
 		cout<<endl;
 		cout << "Detalhes da corrida:" << endl;
-		cout << sapo[0].getname()<<": "<< "Pulos: "<<sapo[0].getpulos()<<" Distância percorrida: "<<sapo1.getdistancia()<<"m"<<endl;
+		cout << sapo[0].getname()<<": "<< "Pulos: "<<sapo[0].getpulos()<<" Distância percorrida: "<<sapo[0].getdistancia()<<"m"<<endl;
 		cout << sapo[1].getname()<<": "<< "Pulos: "<<sapo[1].getpulos()<<" Distância percorrida no momento da chegada do sapo1: "<<sapo[1].getdistancia()<<"m"<<endl;
 		cout << sapo[2].getname()<<": "<< "Pulos: "<<sapo[2].getpulos()<<" Distância percorrida no momento da chegada do sapo1: "<<sapo[2].getdistancia()<<"m"<<endl;
 	}
 	if(ganhador==2){
 		cout <<"***************************************************"<<endl;
-		cout << "O sapo " << sapo[1].getname() << " venceu com " << sapo2.getpulos() <<" pulos e percorreu "<< sapo2.getdistancia() << " metros" << endl;
+		cout << "O sapo " << sapo[1].getname() << " venceu com " << sapo[1].getpulos() <<" pulos e percorreu "<< sapo[1].getdistancia() << " metros" << endl;
 		cout <<"***************************************************"<<endl;
 		cout << "Detalhes da corrida:" << endl;
 		cout<<endl;

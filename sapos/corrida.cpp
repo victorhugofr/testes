7eaa645b212b3901cf52d1e442033d46f624d00a
victorhugofr <32314062+victorhugofr@users.noreply.github.com>
2018-03-27 19:41:58 +0000

    Update corrida.cpp

--- sapos/corrida.cpp
+++ sapos/corrida.cpp
@@ -2,32 +2,33 @@
 #include <ctime> 
 #include <cstdlib>
 #include "sapo.h"
+using namespace std;
+
+
 int corrida(int distancia, Sapo *sapo){
 	int	d1=0,d2=0,d3=0,pulo1=0,pulo2=0,pulo3=0,ganhador;
-	srand(time(0)); 
 	while(d1<distancia || d2 <distancia || d3<distancia){
 		/**A distancia do pulo será entre 1m e 3m*/
-		d1+=rand()%3+1;
-		sapo[0].setdistancia(d1); // determinacao da distancia do pulo no sapo1
-		pulo1++; // contador de quantos pulos ele deu
+		d1+=sapo[0].pular();
+		sapo[0].setdistancia(d1);
+		pulo1++;
 		sapo[0].setpulos(pulo1);
 		if(d1>=distancia){
 			ganhador=1;
 			break;
 		}
-
-		d2+=rand()%3+1;
+		d2+=sapo[1].pular();
 		sapo[1].setdistancia(d2);
-		pulo2++;
+		pulo2++; // contador de quantos pulos ele deu
 		sapo[1].setpulos(pulo2);
 		if(d2>=distancia){
 			ganhador=2;
 			break;
 		}
 
-		d3+=rand()%3+1;
+		d3+=sapo[2].pular();
 		sapo[2].setdistancia(d3);
-		pulo3++;
+		pulo3++; // contador de quantos pulos ele deu
 		sapo[2].setpulos(pulo3);
 		if(d3>=distancia){
 			ganhador=3;

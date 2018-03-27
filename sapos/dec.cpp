dd098d4817b30ba00a1d33678ccb6ed4ba14f1de
victorhugofr <32314062+victorhugofr@users.noreply.github.com>
2018-03-27 19:42:16 +0000

    Update dec.cpp

--- sapos/dec.cpp
+++ sapos/dec.cpp
@@ -1,6 +1,8 @@
 #include "sapo.h"
 #include <iostream>
 #include <cstring>
+#include <ctime> 
+#include <cstdlib>
 
 int Sapo::getpulos(){ return pulos;} /**Funcao que retorna a quantidade de pulos da classe requisitada*/
 void Sapo::setpulos(int p){pulos = p;} /**Funcao que determina a quantidade de pulos da classe requisitada*/
@@ -8,3 +10,9 @@ int Sapo::getdistancia(){ return distancia;} /**Funcao que retorna a distância 
 void Sapo::setdistancia(int d){distancia = d;} /**Funcao que determina a distância percorrida da classe requisitada*/
 char* Sapo::getname(){return nome;}
 void Sapo::setname(char*name){nome=name;}
+int Sapo::pular(){
+	int d;
+		srand(time(NULL));
+		d=rand()%3+1;
+return d;
+}
